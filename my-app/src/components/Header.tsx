eea7d9cd082e31161087888c8d88387cf78f4c38
Youssef Kamel <youssef@Youssefs-Air.home.local>
2025-09-15 21:41:59 -0700

    Create initial UI for chef claude app

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "../assets/chef-claude.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
